4ef94775b1eb3d8fdbc6b1a7dcaf4bc8755dcf92
Morgan Buntin <mmbuntin@gmail.com>
2022-07-04 15:52:27 -0400

    fleshed out boardForm

--- inspo-board/src/components/NewBoardForm.js
+++ inspo-board/src/components/NewBoardForm.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewBoardForm = () => {
+  const [formFields, setFormFields] = useState({
+    title: "",
+    owner: "",
+  });
+
+  const onTitleChange = (event) => {
+    setFormFields({ ...formFields, title: event.target.value });
+  };
+
+  const onOwnerChange = (event) => {
+    setFormFields({ ...formFields, owner: event.target.value });
+  };
+
   return (
     <form>
+      <label htmlFor="boardTitle">Title:</label>
+      <input
+        name="boardTitle"
+        value={formFields.title}
+        onChange={onTitleChange}
+      />
+      <label htmlFor="boardOwner">Owner:</label>
+      <input
+        name="boardOwner"
+        value={formFields.owner}
+        onChange={onOwnerChange}
+      />
       <button type="button"></button>
     </form>
   );
